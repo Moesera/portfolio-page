b6ecf2bac9aa0632f30e444bed504a0ad16b8623
Moesera <89355439+Moesera@users.noreply.github.com>
2024-06-03 00:01:23 +0200

    initial pages

---
+++ src/pages/templates/index.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+const Templates = () => {
+  const [count, setCount] = useState(0);
+
+  const counter = (prop) => {
+    if (prop === "clear") {
+      return setCount(0);
+    }
+
+    return setCount(count + 1);
+  };
+
+  return (
+    <>
+      <h1>Basic starter template</h1>
+      <section>
+        <h2>Template Includes:</h2>
+      </section>
+      <section>
+        <ul>
+          <li>Styled-components</li>
+          <li>React-router-dom</li>
+          <li>Redux/toolkit</li>
+          <li>Sass</li>
+          <li>Layout folder & file setup</li>
+        </ul>
+      </section>
+      <section>
+        <h2>Basic useSatet example:</h2>
+        <button onClick={() => counter("count")}>
+          Counter is currently {count}
+        </button>
+        <button onClick={() => counter("clear")}>Clear Count</button>
+      </section>
+    </>
+  );
+};
+
+export default Templates;
