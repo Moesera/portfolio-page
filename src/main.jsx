f5e8cba7ba6776861299e5ddd92ccb25a3f1eb20
Moesera <89355439+Moesera@users.noreply.github.com>
2024-06-03 00:00:39 +0200

    Routes and global styling for styled-components

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+
+import GlobalStyle from './styled-components/GlobalStyle.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <BrowserRouter>
+    <GlobalStyle />
     <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
