14fddc3ae9ca37d1c71136d9c029d6fbc1771949
Moesera <89355439+Moesera@users.noreply.github.com>
2024-06-04 20:40:31 +0200

    basic styles for components

--- src/styled-components/App.styles.js
+++ src/styled-components/App.styles.js
@@ -1,2 +1,16 @@
 import styled from "styled-components";
 
+export const Header = styled.header`
+background: papayawhip;
+`;
+
+export const Title = styled.h1`
+color: #BF4F74;
+`;
+
+export const Footer = styled.footer`
+background: papayawhip;
+`;
+
+
+
