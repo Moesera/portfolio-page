a86cb4a8efd2eb4c38f5ea3bd3d90bfbe556c213
Moesera <89355439+Moesera@users.noreply.github.com>
2024-06-04 20:40:42 +0200

    basic page styling

--- src/styled-components/GlobalStyle.jsx
+++ src/styled-components/GlobalStyle.jsx
@@ -1,11 +1,40 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+/* Page size */
 :root {
-
+  background-color: #119821;
 };
 
+* {
+  box-sizing: border-box;
+}
 
+html,
+#root,
+body {
+  height: 100%;
+}
+
+#root {
+  display: flex;
+  flex-direction: column;
+}
+
+main, body {
+  margin: 0;
+}
+
+/* General style */
+body {
+  font-family: Arial, sans-serif;
+  background-color: #f8f8f8;
+  color: #333;
+}
+
+main {
+flex: 1;
+}
 
 `;
 
