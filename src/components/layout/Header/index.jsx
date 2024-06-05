918b519c581bc3bcd9299fff91f3f2e4066b5ae6
Moesera <89355439+Moesera@users.noreply.github.com>
2024-06-05 20:42:02 +0200

    Styled components

--- src/components/layout/Header/index.jsx
+++ src/components/layout/Header/index.jsx
@@ -1,15 +1,16 @@
 import Nav from "../Nav";
 
-import * as S from '../../../styled-components/App.styles';
-
+import * as S from "../../../styled-components/App.styles";
 
 const Header = () => {
   return (
-<S.Header>
-  <span>Logo</span>
-<Nav />
-</S.Header>
-  )
-}
+    <S.Header>
+      <S.Wrapper>
+        <span>Logo</span>
+        <Nav />
+      </S.Wrapper>
+    </S.Header>
+  );
+};
 
 export default Header;
--- src/components/layout/Nav/index.jsx
+++ src/components/layout/Nav/index.jsx
@@ -1,17 +1,17 @@
 import { Link } from "react-router-dom";
 
-// import * as S from './styled-components/App.styles';
+import * as S from '../../../styled-components/App.styles';
 
 const Nav = () => {
   return (
-    <nav>
+    <S.Nav>
       <ul>
         <Link>Portfolio</Link>
         <Link>Projects</Link>
         <Link>Hire me</Link>
         <Link>Templates</Link>
       </ul>
-    </nav>
+    </S.Nav>
   )
 }
 
