18c2de9a1cd0268b3149bddb415a6f5f37309305
Moesera <89355439+Moesera@users.noreply.github.com>
2024-06-05 20:42:21 +0200

    background color props for header

--- src/styled-components/App.styles.js
+++ src/styled-components/App.styles.js
@@ -1,15 +1,23 @@
 import styled from "styled-components";
 
 export const Header = styled.header`
-background: papayawhip;
+background: ${(props) => props.theme.color.primary};
 `;
 
 export const Title = styled.h1`
 color: #BF4F74;
 `;
 
+export const Nav = styled.nav`
+background: grey;
+`;
+
 export const Footer = styled.footer`
 background: papayawhip;
+`;
+
+export const Wrapper = styled.div`
+display: flex;
 `;
 
 
