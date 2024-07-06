c96b39c311c58e8e76c51b288bfee8992f75fe21
Moesera <89355439+Moesera@users.noreply.github.com>
2024-07-06 12:54:03 +0200

    list items around the links

--- src/components/layout/Nav/index.jsx
+++ src/components/layout/Nav/index.jsx
@@ -5,10 +5,18 @@ const Nav = () => {
     <S.Nav>
       <S.Logo>Logo</S.Logo>
       <ul>
+        <li>
         <S.Link to="/">Portfolio</S.Link>
+        </li>
+        <li>
         <S.Link to="projects">Projects</S.Link>
+        </li>
+        <li>
         <S.Link to="about">Hire me</S.Link>
+        </li>
+        <li>
         <S.Link to="templates">Templates</S.Link>
+        </li>
       </ul>
     </S.Nav>
   )
