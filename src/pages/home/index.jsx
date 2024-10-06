fc68f63dbc65e69948a5d4ee010269ca876e36eb
Moesera <89355439+Moesera@users.noreply.github.com>
2024-10-06 12:27:10 +0200

    Skills images added

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,23 @@
-const Home = () => {
+import * as S from "../../styled-components/App.styles";
+import { skills } from "../../js/lists/skills";
 
+const Home = () => {
+  
+  console.log(skills);
   return (
-      <h1>React Template</h1>
+    <>
+      <h1>Portfolio</h1>
+      <S.CustomContainer $margin="0 auto" $maxWidth="90%" $justify="center">
+      <h2>Skills</h2>
+      <div>
+        {skills.map((skill, index) => {
+          return (
+          <svg key={index} xmlns={skill.icon} width={50} height={50} viewBox="0 0 24 24" />
+          )
+        })}
+      </div>
+      </S.CustomContainer>
+    </>
   )
 }
 
