a88283486f14b6ba7630cb761f82bfe28fbe2362
Moesera <89355439+Moesera@users.noreply.github.com>
2024-10-22 17:38:44 +0200

    cleanup

--- src/styled-components/GlobalStyle.jsx
+++ src/styled-components/GlobalStyle.jsx
@@ -5,11 +5,6 @@ import * as v from "./variables";
 // ${({theme}) => (theme ? theme.primary : defaultMode.primary)};
 
 const GlobalStyle = createGlobalStyle`
-/* General style */
-/* :root {
-  background-color: ${({theme}) => (theme ? theme.primary : v.defaultMode.primary)};
-}; */
-
 /* VARIABLES */
 html {
   /* Light theme */
@@ -29,41 +24,26 @@ html {
   /* Other */
 }
 
-/* * {
-  box-sizing: border-box;
-} */
-
-  html, 
-  body,
-  #root {
-    min-height: 100%;
-    min-width: 100%;
-    margin: 0 auto;
-  }
-
-  /* *,
-  *::before,
-  *::after {
-    box-sizing: border-box;
-  } */
-
-  /* * {
-    margin: 0;
-    padding: 0;
-    font: inherit;
-  } */
+/* General style */
+html, 
+body,
+#root {
+  min-height: 100%;
+  min-width: 100%;
+  margin: 0 auto;
+}
 
 /* Application Structure */
 body {
   padding: 0;
     font-family: Arial, sans-serif;
-  }
+}
 
 main {
   min-height: 100vh;
 }
 
-/* Footer  */
+// Footer  
 footer {
   padding: 2rem 0;
   /* background-color: ${({theme}) => (theme ? theme.primary : v.defaultMode.secondary)}; */
@@ -85,31 +65,30 @@ h1 {
   justify-self: center;
 }
 
+/* Small screens */
+@media (min-width: ${({ theme }) => theme.screen.mobile}) {
+}
 
-  /* Small screens */
-  @media (min-width: ${({ theme }) => theme.screen.mobile}) {
-  }
-
-  /* Medium screens */
-  @media (min-width: ${({ theme }) => theme.screen.tablet}) {
+/* Medium screens */
+@media (min-width: ${({ theme }) => theme.screen.tablet}) {
     .css-skills {
     grid-template-columns: repeat(auto-fit, minmax(calc(100% / 4), auto));
   }
-  }
+}
 
-  /* Large screens */
-  @media (min-width: ${({ theme }) => theme.screen.desktop}) {
-    .css-skills {
-    grid-template-columns: repeat(auto-fit, minmax(calc(100% / 7), 1fr));
+/* Large screens */
+@media (min-width: ${({ theme }) => theme.screen.desktop}) {
+  .css-skills {
+  grid-template-columns: repeat(auto-fit, minmax(calc(100% / 7), 1fr));
   }
-  }
+}
 
-  /* Extra large screens */
-  @media (min-width: ${({ theme }) => theme.screen.largeDesktop}) {
-    .css-skills {
-    grid-template-columns: repeat(auto-fit, minmax(calc(100% / 20), 1fr));
+/* Extra large screens */
+@media (min-width: ${({ theme }) => theme.screen.largeDesktop}) {
+  .css-skills {
+  grid-template-columns: repeat(auto-fit, minmax(calc(100% / 20), 1fr));
   }
-  }
+}
 `;
 
 export default GlobalStyle;
